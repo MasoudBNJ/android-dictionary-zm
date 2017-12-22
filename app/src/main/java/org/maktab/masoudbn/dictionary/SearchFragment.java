b3676985377afe7384c8c18a563e096f5e088145
mmahdi2324 <m.mahdizakeri@gmail.com>
2017-12-23 00:17:32 +0330

    design add fragment layout and search fragment layout

--- app/src/main/java/org/maktab/masoudbn/dictionary/SearchFragment.java
+++ app/src/main/java/org/maktab/masoudbn/dictionary/SearchFragment.java
@@ -3,6 +3,7 @@ package org.maktab.masoudbn.dictionary;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +13,7 @@ import android.view.ViewGroup;
  * A simple {@link Fragment} subclass.
  */
 public class SearchFragment extends Fragment {
-
+        private RecyclerView recyclerView;
 
     public SearchFragment() {
         // Required empty public constructor
@@ -23,7 +24,12 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        recyclerView = (RecyclerView) view.findViewById(R.id.my_recycler_view);
+
+
+
+        return view;
     }
 
 }
